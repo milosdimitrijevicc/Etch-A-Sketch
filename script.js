35841aaa96b150c579d81a16e6167787a57e283f
Milos Dimitrijevic <dimitrijevic.milos40@gmail.com>
2024-03-21 10:29:10 +0100

    update

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
+//Varijable
 const grid = document.querySelector('.grid');
-const cell = document.querySelector('.cell');
 
+// Math.floor(Math.random() * 101);
 
+//State
 const sketchCreator = () => {
+  let currentColor = "#3C9EE7";
+  const getCurrentColor = () => currentColor;
+  const setCurrentColor = (color) => currentColor = color;
   
+  const setErasor = () => currentColor = '#ffffff';
+  const setClear = () => currentColor = '#ffffff';
+
+  //random color
+  const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+  const randomByte = () => randomNumber(0, 255);
+  const randomRGB = () => `rgb(${[randomByte(), randomByte(), randomByte()].join(',')})`
+  const setRandomColor = () => currentColor = randomRGB();
+
+
+  return {getCurrentColor, setCurrentColor, setErasor, setClear, randomNumber, randomByte, randomRGB, setRandomColor}
 }
+const game = sketchCreator();
 
 
 
+//Kreiranje 16 divova 
+const createGrid = (number) =>{
 
+  for (let i = 0; i < number; i++) {
+    for (let j = 0; j < number; j++) {
+      grid.style.display = 'grid';
+      grid.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+      grid.style.gridTemplateRows = `repeat(${number}, 1fr)`;
 
-
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    grid.appendChild(cell);
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      grid.appendChild(cell);
+  
+      cell.addEventListener('mouseover' , function(){
+        cell.style.backgroundColor = game.getCurrentColor();
+      })
+    }
   }
-}
+};
 
+createGrid(4);
 
-cell.addEventListener('mouseover' , function(){
-  cell.style.backgroundColor = '#3C9EE7';
-})
+// const range = document.querySelector('.inputRange');
+// const value = document.querySelector('.currentGridText');
+// console.log(range.value);
+// value.textContent = range.value;
+
+const value = document.querySelector("#value");
+const input = document.querySelector("#rangeInput");
+value.textContent = input.value;
+input.addEventListener("input", (event) => {
+  value.textContent = event.target.value;
+});
+
